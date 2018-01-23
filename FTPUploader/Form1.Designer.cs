e7e3c3bc53465b2b95181e0c5b133db1a4a2bfe9
REZA <REZA@REZA-PC>
2018-01-23 11:12:59 +0700

    Add project files.

---
+++ FTPUploader/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace FTPSUploader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button_start = new System.Windows.Forms.Button();
+            this.textbox_username = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textbox_password = new System.Windows.Forms.TextBox();
+            this.textbox_direktori = new System.Windows.Forms.TextBox();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.textbox_url = new System.Windows.Forms.TextBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button_start
+            // 
+            this.button_start.Location = new System.Drawing.Point(261, 201);
+            this.button_start.Name = "button_start";
+            this.button_start.Size = new System.Drawing.Size(100, 26);
+            this.button_start.TabIndex = 0;
+            this.button_start.Text = "Start";
+            this.button_start.UseVisualStyleBackColor = true;
+            this.button_start.Click += new System.EventHandler(this.button_start_Click);
+            // 
+            // textbox_username
+            // 
+            this.textbox_username.Enabled = false;
+            this.textbox_username.Location = new System.Drawing.Point(41, 65);
+            this.textbox_username.Name = "textbox_username";
+            this.textbox_username.Size = new System.Drawing.Size(195, 20);
+            this.textbox_username.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pictureBox4);
+            this.groupBox1.Controls.Add(this.textbox_url);
+            this.groupBox1.Controls.Add(this.textbox_direktori);
+            this.groupBox1.Controls.Add(this.pictureBox3);
+            this.groupBox1.Controls.Add(this.textbox_password);
+            this.groupBox1.Controls.Add(this.pictureBox2);
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Controls.Add(this.textbox_username);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(349, 168);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = " FTP Settings ";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // textbox_password
+            // 
+            this.textbox_password.Enabled = false;
+            this.textbox_password.Location = new System.Drawing.Point(41, 92);
+            this.textbox_password.Name = "textbox_password";
+            this.textbox_password.PasswordChar = '*';
+            this.textbox_password.Size = new System.Drawing.Size(195, 20);
+            this.textbox_password.TabIndex = 5;
+            this.textbox_password.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textbox_direktori
+            // 
+            this.textbox_direktori.Enabled = false;
+            this.textbox_direktori.Location = new System.Drawing.Point(41, 121);
+            this.textbox_direktori.Name = "textbox_direktori";
+            this.textbox_direktori.Size = new System.Drawing.Size(290, 20);
+            this.textbox_direktori.TabIndex = 7;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseClick);
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // textbox_url
+            // 
+            this.textbox_url.Enabled = false;
+            this.textbox_url.Location = new System.Drawing.Point(41, 35);
+            this.textbox_url.Name = "textbox_url";
+            this.textbox_url.Size = new System.Drawing.Size(195, 20);
+            this.textbox_url.TabIndex = 8;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackgroundImage = global::FTPSUploader.Properties.Resources.link_26;
+            this.pictureBox4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox4.Location = new System.Drawing.Point(16, 37);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(14, 14);
+            this.pictureBox4.TabIndex = 9;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackgroundImage = global::FTPSUploader.Properties.Resources.active_directory_26;
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox3.Location = new System.Drawing.Point(16, 122);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(14, 14);
+            this.pictureBox3.TabIndex = 6;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = global::FTPSUploader.Properties.Resources.password2_26;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Location = new System.Drawing.Point(16, 94);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(14, 14);
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::FTPSUploader.Properties.Resources.user_26;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(16, 67);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(14, 14);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 240);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(372, 22);
+            this.statusStrip1.TabIndex = 4;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(39, 17);
+            this.toolStripStatusLabel1.Text = "Ready";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(372, 262);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button_start);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FTP Uploader";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_start;
+        private System.Windows.Forms.TextBox textbox_username;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textbox_password;
+        private System.Windows.Forms.TextBox textbox_direktori;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.TextBox textbox_url;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ FTPUploader/Form1.cs
@@ -0,0 +1,460 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AlexPilotti.FTPS.Client;
+using AlexPilotti.FTPS.Common;
+using System.Security.Cryptography.X509Certificates;
+using System.Net.Security;
+
+namespace FTPSUploader
+{
+    public partial class Form1 : Form
+    {
+        string username = "username";
+        string password = "password";
+        string urlftp = "ft.toyota.astra.co.id";
+        int port = 21;
+
+        string lokasiFileINI = "";
+        string namaFileINI = "Settings.ini";
+
+        string jadwal = "";
+        string jam = "";
+        string direktoritujuan = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bacaFileINI(string lokasiFile)
+        {
+            var lines = File.ReadLines(lokasiFile);
+            StringBuilder strb = new StringBuilder();
+            foreach (var line in lines)
+            {
+                Console.WriteLine("" + line);
+                strb.Append(line);
+            }
+
+            string data = strb.ToString();
+
+            bool sesuaiFormat = false;
+            try
+            {
+                sesuaiFormat = Regex.IsMatch(data, "[\\w\\W\\s]*[\\[]FTPSETTING[\\]][\\w\\W\\s]*[\\[]ENDFTPSETTING[\\]][\\w\\W\\s]*");
+                if (sesuaiFormat)
+                {
+                    try
+                    {
+                        Regex RegexObj = new Regex("[\\w\\W\\s]*[\\[]FTPSETTING[\\]][\\w\\W\\s]*[\\[]Schedule[\\]][\\s]*[\\=][\\s]*[\\\"]([\\w\\W\\s]*)[\\\"][\\w\\S\\s]*[\\[]Destination[\\]][\\s]*[\\=][\\s]*[\\\"]([\\w\\s\\W]*)[\\\"][\\w\\W\\s]*[\\[]FTPHost[\\]][\\s]*[\\=][\\s]*[\\\"]([\\w\\W\\s]*)[\\\"][\\w\\W\\s]*[\\[]Username[\\]][\\s]*[\\=][\\s]*[\\\"]([\\w\\W\\s]*)[\\\"][\\w\\W\\s]*[\\[]Password[\\]][\\s]*[\\=][\\s]*[\\\"]([\\w\\W\\s]*)[\\\"][\\w\\W\\s]*[\\[]ENDFTPSETTING[\\]][\\w\\W\\s]*");
+                        jadwal = RegexObj.Match(data).Groups[1].Value;
+                        direktoritujuan = RegexObj.Match(data).Groups[2].Value;
+                        urlftp = RegexObj.Match(data).Groups[3].Value;
+                        username = RegexObj.Match(data).Groups[4].Value;
+                        password = RegexObj.Match(data).Groups[5].Value;
+
+                        Console.WriteLine("" + jadwal);
+                        Console.WriteLine("" + direktoritujuan);
+                        Console.WriteLine("" + urlftp);
+                        Console.WriteLine("" + username);
+                        Console.WriteLine("" + password);
+
+                        Regex rgxx = new Regex(":");
+                        string[] dt = rgxx.Split("" + jadwal);
+                        jam = dt[0];
+
+                        Console.WriteLine("" + jam);
+                    }
+                    catch (ArgumentException ex)
+                    {
+                    }
+                }
+            }
+            catch (ArgumentException ex)
+            {
+            }
+        }
+
+        private void SimpanFileINI(string lokasiFile)
+        {
+            StreamWriter writetext = new StreamWriter(lokasiFile);
+
+            StringBuilder strbuild = new StringBuilder();
+            strbuild.Append("[FTPSETTING]\r\n");
+            strbuild.Append("[Schedule]=\"17:00:10\"\r\n");
+            strbuild.Append("[Destination]=\"/INTERFACE/\"\r\n");
+            strbuild.Append("[FTPHost]=\"" + urlftp + "\"\r\n");
+            strbuild.Append("[Username]=\"" + username + "\"\r\n");
+            strbuild.Append("[Password]=\"" + password + "\"\r\n");
+            strbuild.Append("[ENDFTPSETTING]\r\n");
+
+            writetext.WriteLine("" + strbuild.ToString());
+            writetext.Close();
+        }
+
+        private void createDirektori()
+        {
+            ///create disik
+            string root2 = lokasiFileINI + "\\Outbox";
+            if (!Directory.Exists(root2))
+            {
+                Directory.CreateDirectory(root2);
+            }
+            ////
+
+            ///create disik
+            string root = lokasiFileINI + "\\SentFile";
+            if (!Directory.Exists(root))
+            {
+                Directory.CreateDirectory(root);
+            }
+            ////
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lokasiFileINI = System.IO.Path.GetDirectoryName(
+               System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase);
+            lokasiFileINI = lokasiFileINI.Replace("file:\\", "");
+
+            createDirektori();
+
+            //cek exists
+            if (File.Exists(lokasiFileINI + "\\" + namaFileINI))
+            {
+                Console.WriteLine("file exists");
+                bacaFileINI(lokasiFileINI + "\\" + namaFileINI);
+            }
+            else
+            {
+                Console.WriteLine("file not exists");
+                SimpanFileINI(lokasiFileINI + "\\" + namaFileINI);
+                bacaFileINI(lokasiFileINI + "\\" + namaFileINI);
+            }
+
+            displayKomponen();
+            START();
+        }
+
+        public void displayKomponen()
+        {
+            textbox_url.Text = "" + urlftp;
+            textbox_username.Text = "" + username;
+            textbox_password.Text = "" + password;
+            textbox_direktori.Text = "" + direktoritujuan;
+        }
+
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MinimzedTray()
+        {
+            notifyIcon1.Visible = true;
+            notifyIcon1.Icon = SystemIcons.Application;
+
+            notifyIcon1.BalloonTipText = "Minimized";
+            notifyIcon1.BalloonTipTitle = "FTP Uploader is running in background";
+            notifyIcon1.ShowBalloonTip(500);
+
+        }
+
+        private void MaxmizedFromTray()
+        {
+            notifyIcon1.Visible = false;
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (FormWindowState.Minimized == this.WindowState)
+            {
+                MinimzedTray();
+            }
+            else if (FormWindowState.Normal == this.WindowState)
+            {
+
+                MaxmizedFromTray();
+            }
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.WindowState = FormWindowState.Normal;
+            Form1 frm = new Form1();
+            frm.Show();
+            MaxmizedFromTray();
+
+
+        }
+
+        private void notifyIcon1_MouseClick(object sender, MouseEventArgs e)
+        {
+            this.WindowState = FormWindowState.Normal;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                e.Cancel = true;
+                this.WindowState = FormWindowState.Minimized;
+                this.ShowInTaskbar = false;
+                this.Hide();
+
+            }
+        }
+
+        private void notifyIcon1_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Normal;
+            notifyIcon1.BalloonTipText = "Normal";
+            notifyIcon1.ShowBalloonTip(500);
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_start_Click(object sender, EventArgs e)
+        {
+            START();
+        }
+
+        public void START()
+        {
+            timer1.Enabled = true;
+            timer1.Start();
+            button_start.Enabled = false;
+            toolStripStatusLabel1.Text = "Running...";
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            DateTime time = DateTime.Now;
+            string format = "HH";
+            string waktu = time.ToString(format);
+
+            int mJam = Convert.ToInt16(waktu);
+            int setJam = Convert.ToInt16(jam);
+
+            if (mJam == setJam)
+            {
+                bacaSemuaFiledariFolder(lokasiFileINI + "\\Outbox");
+            }
+        }
+
+        private bool IsFileLocked(string filename)
+        {
+            FileInfo file = new FileInfo(filename);
+            FileStream stream = null;
+
+            try
+            {
+                stream = file.Open(FileMode.Open, FileAccess.ReadWrite, FileShare.None);
+            }
+            catch (IOException)
+            {
+                return true;
+            }
+            finally
+            {
+                if (stream != null)
+                    stream.Close();
+            }
+
+            //file is not locked
+            return false;
+        }
+        public void MoveFile(string from, string to)
+        {
+            try
+            {
+                FileInfo file = new FileInfo(from);
+                // check if the file exists
+
+                if (file.Exists)
+                {
+                    // check if the file is not locked
+                    if (IsFileLocked(from) == false)
+                    {
+                        // move the file
+                        File.Move(from, to);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                ;
+            }
+        }
+        private void bacaSemuaFiledariFolder(string folder)
+        {
+            foreach (string txtName in Directory.GetFiles(folder, "*"))
+            {
+                Console.WriteLine(txtName.ToString());
+                uploadFileFTPS(txtName.ToString());
+            }
+
+            //sukses
+
+            DateTime time = DateTime.Now;
+            string format = "yyyy-MM-dd HH";
+            string waktu = time.ToString(format);
+
+            string dirtujuan = lokasiFileINI + "\\SentFile\\" + waktu;
+            ///create disik
+            string root = dirtujuan;
+            if (!Directory.Exists(root))
+            {
+                Directory.CreateDirectory(root);
+            }
+            ////
+
+            ///baru pindahkan
+            //Console.WriteLine("Pindahkan");
+            foreach (string txtName in Directory.GetFiles(folder, "*"))
+            {
+                bool sesuaiFormat = false;
+                try
+                {
+                    string namafile = "";
+                    sesuaiFormat = Regex.IsMatch(txtName, "[\\w\\W\\s]*Outbox[\\\\]([\\w\\W\\s]*)");
+                    if (sesuaiFormat)
+                    {
+                        try
+                        {
+                            Regex RegexObj = new Regex("[\\w\\W\\s]*Outbox[\\\\]([\\w\\W\\s]*)");
+                            namafile = RegexObj.Match(txtName).Groups[1].Value;
+
+                            Console.WriteLine("" + namafile);
+
+                            MoveFile(folder + "\\" + namafile, dirtujuan + "\\" + namafile);
+                        }
+                        catch (ArgumentException ex)
+                        {
+                        }
+                    }
+                }
+                catch (ArgumentException ex)
+                {
+                }
+
+
+            }
+        }
+
+        private void uploadFileFTPS(string lokasifile)
+        {
+            using (FTPSClient client = new FTPSClient())
+            {
+                string namafile = "";
+                //ambil nama filenya
+                bool sesuaiFormat = false;
+                try
+                {
+                    sesuaiFormat = Regex.IsMatch(lokasifile, "[\\w\\W\\s]*Outbox[\\\\]([\\w\\W\\s]*)");
+                    if (sesuaiFormat)
+                    {
+                        try
+                        {
+                            Regex RegexObj = new Regex("[\\w\\W\\s]*Outbox[\\\\]([\\w\\W\\s]*)");
+                            namafile = RegexObj.Match(lokasifile).Groups[1].Value;
+                        }
+                        catch (ArgumentException ex)
+                        {
+                        }
+                    }
+                }
+                catch (ArgumentException ex)
+                {
+                }
+
+                Console.WriteLine("Namafile:"+namafile);
+                Console.WriteLine("Lokasifile:" + lokasifile);
+
+                FileInfo objFile = new FileInfo(lokasifile);
+                Console.WriteLine("" + objFile.Name);               
+
+                try
+                {
+                    client.Connect(urlftp, new NetworkCredential(username, password),
+                                                  ESSLSupportMode.All, new RemoteCertificateValidationCallback(ValidateTestServerCertificate));
+                    Console.WriteLine("" + objFile.Directory);
+
+                    //mulai upload
+                    client.PutFile(lokasifile, direktoritujuan + namafile); 
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(""+e.StackTrace);
+                }
+               
+            }
+        }
+
+        private static bool ValidateTestServerCertificate(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+        {
+            if (sslPolicyErrors == SslPolicyErrors.None)
+                return true;
+            else
+            {
+                return false;
+            }
+
+        }
+
+        private void uploadFileFTP(string lokasifile)
+        {
+            FileInfo objFile = new FileInfo(lokasifile);
+            FtpWebRequest objFTPRequest;
+
+            objFTPRequest = (FtpWebRequest)FtpWebRequest.Create(new Uri("ftp://" + urlftp + direktoritujuan + objFile.Name));
+            objFTPRequest.Credentials = new NetworkCredential(username, password);
+            objFTPRequest.KeepAlive = false;
+            objFTPRequest.UseBinary = true;
+            objFTPRequest.ContentLength = objFile.Length;
+            objFTPRequest.Method = WebRequestMethods.Ftp.UploadFile;
+            int intBufferLength = 16 * 1024;
+            byte[] objBuffer = new byte[intBufferLength];
+            FileStream objFileStream = objFile.OpenRead();
+
+            try
+            {
+                Stream objStream = objFTPRequest.GetRequestStream();
+
+                int len = 0;
+
+                while ((len = objFileStream.Read(objBuffer, 0, intBufferLength)) != 0)
+                {
+                    objStream.Write(objBuffer, 0, len);
+                }
+
+                objStream.Close();
+                objFileStream.Close();
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
